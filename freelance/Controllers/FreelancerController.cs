967c2e7043fe1ee2a68838f1851b178ea50d8690
Abdallh Mohammed <absallh43@gmail.com>
2021-06-05 02:15:12 +0200

    smi-ending gui

--- freelance/Controllers/FreelancerController.cs
+++ freelance/Controllers/FreelancerController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using w.Models;
 
 namespace freelance.Controllers
 {
@@ -11,6 +13,11 @@ namespace freelance.Controllers
     {
         // GET: Freelancer
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EditProfile()
         {
             return View();
         }
---
+++ freelance/Scripts/js/freelancerSearch.js
@@ -0,0 +1,30 @@
+﻿function searchToggle(obj, evt) {
+    var container = $(obj).closest('.search-wrapper');
+    if (!container.hasClass('active')) {
+        container.addClass('active');
+        evt.preventDefault();
+    }
+    else if (container.hasClass('active') && $(obj).closest('.input-holder').length == 0) {
+        container.removeClass('active');
+        // clear input
+        container.find('.search-input').val('');
+    }
+}
+
+let searchBar = document.getElementById('postsSearch');
+let posts = document.querySelectorAll('.card');
+searchBar.addEventListener("keyup", function (event) {
+    const q = event.target.value.toLowerCase();
+    posts.forEach((post) => {
+        console.log(post.querySelector('.description p').textContent);
+        if (post.querySelector('.description p').textContent.toLowerCase().includes(q) ||
+            post.querySelector('.description h1').textContent.toLowerCase().includes(q) ||
+            post.querySelector('.description h2').textContent.toLowerCase().includes(q) ||
+            post.querySelector('.description h4').textContent.toLowerCase().includes(q)) {
+            post.className = 'card';
+        } else {
+            post.className = 'holeOut';
+        }
+    });
+});
+console.log(posts);
