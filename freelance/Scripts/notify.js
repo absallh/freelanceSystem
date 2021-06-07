ec1388d657127fdefb9e05cf3323e75a74a0f756
Abdallh Mohammed <absallh43@gmail.com>
2021-06-07 10:15:01 +0200

    final

---
+++ freelance/Scripts/notify.js
@@ -0,0 +1,10 @@
+﻿const btn = document.querySelector('.submit-button');
+const form = document.querySelector('.add-product');
+const msg = document.querySelector('#success');
+
+btn.addEventListener('click', () => {
+    console.log('fasdfasdf');
+    form.classList.add('boingOutDown');
+    msg.classList.add('boingInUp');
+    msg.classList.add('pointerEvent');
+});
